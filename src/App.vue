18ff10a43ec3f02a4bdc2dc2fe6729f123e8fc54
Naydyonov <naydyonov81@gmail.com>
2019-02-14 23:38:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,154 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="search-panel">
+      <iata-select
+              :requestHeader="requestHeader"
+              :label="labelDeparture"
+              @checkAirport="checkIataDeparture"
+      >
+      </iata-select>
+      <iata-select
+              :requestHeader="requestHeader"
+              :label="labelArrival"
+              @checkAirport="checkIataArrival"
+      ></iata-select>
+      <div class="class-select">
+        <template v-for="(fClass, index) in flightClasses">
+          <label class="class-select__item">
+            <input type="radio" :value="fClass" v-model="selectedClass">
+            <span class="class-select__name" >{{fClass.name}}</span>
+          </label>
+        </template>
+      </div>
+      <datepicker v-modal="date"></datepicker>
+      <div class="button" @click="searchClick">Поиск</div>
+    </div>
+    <div class="search-params">
+      <div class="search-params__title">Параметры поиска</div>
+      <div class="search-params__item">Departure  - {{ iataDeparture }} </div>
+      <div class="search-params__item">Arriaval - {{ iataArrival }}</div>
+      <div class="search-params__item">Date - {{ date}}</div>
+      <div class="search-params__item">Flight Class - {{ selectedClass.name }} ( {{selectedClass.code}} )</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+ // import HelloWorld from './components/Offers'
+ import IATASelect from './components/IATAselect'
+
+ const app_id = '8e71ac9f18';
+
+export default {
+    name: 'app',
+    components: {
+    // HelloWorld
+    'iata-select': IATASelect
+    },
+    data () {
+        return  {
+            loginResult: [],
+            api_key: '',
+            request_id: '',
+            requestHeader: [],
+            iataDeparture: '',
+            iataArrival:'',
+            flightClass: '',
+            flightClasses: [
+                {name: 'Эконом', code: 'E'},
+                {name: 'Бизнес', code: 'B'},
+            ],
+            selectedClass: [],
+            labelDeparture: 'IATA departure',
+            labelArrival: 'IATA arrival',
+            date: '',
+            searchQuery: {
+                directions: [
+                    {
+                        departure_code: '',
+                        arrival_code: '',
+                        date: "2019-02-21",
+                        time: "M"
+                    }
+                ],
+                adult_qnt: 1,
+                child_qnt: 0,
+                infant_qnt: 0,
+                passenger_category: "",
+                class: "E",
+                direct: true,
+                flexible: true,
+                max_price: '10000000',
+                airlines: [],
+                fare_types: []
+            },
+            urlSearch: 'https://api-stage.etm-system.com/api/air/search',
+        }
+    },
+    created: function () {
+        if(!localStorage.api_key) {
+            this.$http.get('https://api-stage.etm-system.com/api/login/' + app_id)
+                .then(function (response) {
+                    localStorage.api_key = response.data.etm_auth_key;
+                })
+        }
+        else {
+            this.api_key = localStorage.api_key;
+        }
+        this.requestHeader = { headers: { "etm-auth-key": this.api_key }};
+
+        console.log(this.api_key);
+        console.log(this.requestHeader);
+    },
+    methods: {
+        transmitRequestID(id) {
+            this.request_id = id;
+            console.log('Родитель получил request_id - ' + this.request_id);
+        },
+        checkIataDeparture(iata) {
+            this.iataDeparture = iata;
+        },
+        checkIataArrival(iata) {
+            this.iataArrival = iata;
+        },
+        searchClick () {
+            console.log('Search request');
+            console.log(this.requestHeader);
+            console.log('Departure - ' + this.iataDeparture);
+            console.log('Arrival - ' + this.iataArrival);
+            console.log('Date - ' + this.date);
+            this.searchQuery.directions[0].departure_code = this.iataDeparture;
+            this.searchQuery.directions[0].arrival_code = this.iataArrival;
+            this.searchQuery.class = this.selectedClass.code;
+            console.log(this.searchQuery);
+            this.$http.post(this.urlSearch, this.searchQuery, this.requestHeader)
+                .then(response => {
+                    console.log('status - ' + response.body.status);
+                    console.log('request_id - ' + response.body.request_id);
+                    this.request_id = response.body.request_id;
+                    // this.$emit('updateRequestID', this.request_id);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        customFormatter(date) {
+            return date.format('MMMM Do YYYY, h:mm:ss a');
+        }
+    }
+
+}
+</script>
+
+<style lang="scss">
+    @import "assets/style";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Offers.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="offers container">
+        <div class="offers__result-status">
+            Найдено: {{this.requestStatus}}
+        </div>
+        <div class="offers__wrap">
+            <div class="offers__list" v-bind:key="index" v-for="(offer, index) in this.offers">
+                <div class="accordion">
+                    <div class="offers__airline airline accordion__visible">
+                        <div class="airline__logo"><img v-bind:src="offer.carrier_logo" alt=""></div>
+                        <div class="airline__name">{{offer.carrier_name}}</div>
+                        <div class="airline__minprice">Цена от {{offer.min_price}}</div>
+                    </div>
+                    <div class="accordion__hide">
+                        <div class="offers__offer offer" v-bind:key="soIndex" v-for="(singleOffer, soIndex) in offer.offers">
+                            <div class="offer__flight-number">Рейс - {{singleOffer.flight_number}}</div>
+                            <div class="offer__airline-name">{{singleOffer.flight_carrier_name}}</div>
+                            <div class="offer__departure-airport">{{singleOffer.departure_airport}}</div>
+                            <div class="offer__departure-time">{{singleOffer.departure_timestamp}}</div>
+                            <div class="offer__arrival-airport">{{singleOffer.arrival_airport}}</div>
+                            <div class="offer__arrival-time">{{singleOffer.arrival_timestamp}}</div>
+                            <div class="offer__duration-time">{{singleOffer.duration_formated}}</div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "offers",
+        props: [
+            'request_id',
+            'api_key'
+        ],
+        data() {
+            return {
+                offers:[],
+                requestStatus: '',
+                requestMsg: '',
+                offers: []
+            }
+        },
+        methods: {
+            updateSource: function () {
+                console.log('Search request');
+                this.$http.get(this.urlOffers, this.requestHeader)
+                    .then(response => {
+                        console.log(response.body);
+                        this.requestStatus = response.body.status;
+                        this.requestMsg = response.body.message;
+                        this.offers = response.body.offers;
+                        console.log('status - ' + this.requestStatus);
+                        console.log('message' + this.requestMsg);
+                        console.log('search results - ' + this.offers);
+                    })
+                    .catch(err => {
+                        console.log('status - ' + err.status);
+                        console.log('error - ' + err.error);
+                    })
+            }
+        },
+        watch: {
+            request_id: function () {
+                this.urlOffers = 'https://api-stage.etm-system.com/api/air/offers?request_id=' + this.request_id + '&sort=price';
+                console.log('request_id - ' + this.request_id);
+                console.log('URL - ' + this.urlOffers);
+                console.log('headers - ' + this.requestHeader);
+                this.updateSource();
+            }
+        }
+    }
+</script>
+
+
+<style scoped lang="scss">
+
+.offers {
+    font-family: Roboto;
+    padding-top: 20px;
+
+    &__results-status {
+        font-size: 16px;
+        font-weight: 500;
+        text-align: center;
+    }
+}
+
+</style>
